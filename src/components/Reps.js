48a7f1650e1979a40ee8b426ce7cbf282d2df33e
David Saunders <david@monkee-boy.com>
2018-03-02 21:57:52 -0600

    Added representatives' image, address, and phone number

--- src/components/Reps.js
+++ src/components/Reps.js
@@ -3,26 +3,44 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { getReps, getDivisionID, getResultsByAddress } from '../actions/actionCreators'
 
-const Reps = props => (
+const Reps = props => {
+
+  const rep = props.data.local_data.representatives.officials;
   
-  <div>
-    <h3>Representatives</h3>
-    {
-      props.data.local_data.representatives.offices.map((office, i) => 
+  return(
+    <div>
+      <h3>Representatives</h3>
+      {
+        props.data.local_data.representatives.offices.map((office, i) => 
 
-      <div key={i}>
+        <div key={i}>
 
-        {office.name}
+          <h4>{office.name}</h4>
 
-        {office.officialIndices.map((index) => 
-            <p key={index}>{props.data.local_data.representatives.officials[index].name}</p>,
-        )}
-          
-      </div>
-      )
-    }
-  </div>
-)
+          {office.officialIndices.map((index) => 
+              <div key={index}>
+                <img width="100" height="100" src={rep[index].photoUrl} alt={rep[index].name}/>
+                <h5>{rep[index].name} - {rep[index].party}</h5>
+                <div className="contact">
+                  <div className="address">
+                    <p>{rep[index].address[0].line1}</p>
+                    <p>{rep[index].address[0].city}, {rep[index].address[0].state} {rep[index].address[0].zip}</p>
+                  </div>
+                  <p>{rep[index].phones}</p>
+                </div>
+                <div className="social">
+                  Facebook
+                  Twitter
+                </div>
+              </div>,
+          )}
+
+        </div>
+        )
+      }
+    </div>
+  )
+}
 
 const mapStateToProps = state => ({
   data: state.getData
